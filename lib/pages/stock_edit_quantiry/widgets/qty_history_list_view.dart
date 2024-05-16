8249fbfbe219848b7e072586e632ad87e3b03ac3
Ravi Kalola <rjkalola@gmail.com>
2024-05-16 12:34:05 +0530

    minor changes

--- lib/pages/stock_edit_quantiry/widgets/qty_history_list_view.dart
+++ lib/pages/stock_edit_quantiry/widgets/qty_history_list_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:otm_inventory/pages/stock_edit_quantiry/stock_edit_quantity_controller.dart';
+import 'package:otm_inventory/pages/stock_edit_quantiry/widgets/note_info.dart';
+import 'package:otm_inventory/pages/stock_edit_quantiry/widgets/price_info.dart';
+import 'package:otm_inventory/pages/stock_edit_quantiry/widgets/user_info.dart';
 import 'package:otm_inventory/utils/string_helper.dart';
 import '../../../../res/colors.dart';
 
@@ -25,22 +28,52 @@ class QtyHistoryListView extends StatelessWidget {
                   //     categoryListController.categoryList[position]);
                 },
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 7, 16, 7),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      customTextView(
-                          stockEditQuantityController.productInfo.value.stock_histories![position].created_at_formatted,
-                          16,
-                          FontWeight.w400,
-                          primaryTextColorLight,
-                          const EdgeInsets.all(0)),
-                      customTextView(
-                          stockEditQuantityController.productInfo.value.stock_histories![position].qty,
-                          16,
-                          FontWeight.w400,
-                          (int.parse(stockEditQuantityController.productInfo.value.stock_histories![position].qty!)>0)?Colors.green:Colors.red,
-                          const EdgeInsets.all(0))
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            customTextView(
+                                stockEditQuantityController.productInfo.value
+                                    .stock_histories![position]
+                                    .created_at_formatted,
+                                16,
+                                FontWeight.w400,
+                                primaryTextColorLight,
+                                const EdgeInsets.all(0)),
+                            customTextView(
+                                stockEditQuantityController.productInfo.value
+                                    .stock_histories![position]
+                                    .qty,
+                                16,
+                                FontWeight.w400,
+                                (int.parse(stockEditQuantityController.productInfo.value
+                                    .stock_histories![position]
+                                    .qty!) >
+                                    0)
+                                    ? Colors.green
+                                    : Colors.red,
+                                const EdgeInsets.all(0)),
+                          ],
+                        ),
+                      ),
+                      QtyHistoryUserInfo(user: stockEditQuantityController.productInfo.value
+                          .stock_histories![position]
+                          .user,),
+                      QtyHistoryPriceInfo(price: stockEditQuantityController.productInfo.value
+                          .stock_histories![position].currencyPrice,),
+                      QtyHistoryNoteInfo(note: stockEditQuantityController.productInfo.value
+                          .stock_histories![position].reference,),
+                      const SizedBox(height: 14,),
+                      const Divider(
+                        thickness: 1,
+                        height: 1,
+                        color: dividerColor,
+                      ),
                     ],
                   ),
                 ),
---
+++ lib/pages/stock_edit_quantiry/widgets/user_info.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:otm_inventory/pages/otp_verification/model/user_info.dart';
+
+import '../../../utils/image_utils.dart';
+import '../../../utils/string_helper.dart';
+import '../../../widgets/text/PrimaryTextView.dart';
+class QtyHistoryUserInfo extends StatelessWidget {
+   QtyHistoryUserInfo({super.key,this.user});
+  final UserInfo? user;
+  @override
+  Widget build(BuildContext context) {
+    return !StringHelper.isEmptyObject(user)
+        ? Padding(
+      padding: const EdgeInsets.fromLTRB(16, 6, 16, 0),
+      child: Row(
+        children: [
+          ImageUtils.setUserImage(
+              user!.image ??
+                  "",
+              36,
+              45),
+          const SizedBox(
+            width: 9,
+          ),
+          PrimaryTextView(
+            text: user!
+                .name ??
+                "",
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          )
+        ],
+      ),
+    )
+        : Container();
+  }
+}
