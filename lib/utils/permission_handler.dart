8a22555043df714bed5490e8bb0401fa7ed7111b
Ravi Kalola <rjkalola@gmail.com>
2024-06-22 18:48:00 +0530

    solved minor issue

--- lib/utils/permission_handler.dart
+++ lib/utils/permission_handler.dart
@@ -7,13 +7,19 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionHandler {
   static Future<bool> isStoragePermission() async {
     final DeviceInfoPlugin info = DeviceInfoPlugin();
-    final AndroidDeviceInfo androidInfo = await info.androidInfo;
-    debugPrint('releaseVersion : ${androidInfo.version.release}');
-    final int androidVersion = int.parse(androidInfo.version.release);
+
     bool havePermission = false;
 
-    if (Platform.isAndroid && androidVersion >= 13) {
-      havePermission = true;
+    if (Platform.isAndroid) {
+      final AndroidDeviceInfo androidInfo = await info.androidInfo;
+      debugPrint('releaseVersion : ${androidInfo.version.release}');
+      final int androidVersion = int.parse(androidInfo.version.release);
+      if (androidVersion >= 13) {
+        havePermission = true;
+      } else {
+        final status = await Permission.storage.request();
+        havePermission = status.isGranted;
+      }
     } else {
       final status = await Permission.storage.request();
       havePermission = status.isGranted;
